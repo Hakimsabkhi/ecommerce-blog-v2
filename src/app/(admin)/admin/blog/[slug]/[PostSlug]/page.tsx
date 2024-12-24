021ca159557524cc436816794c403bd0f98b68f6
hakim <hakimsabkhi001@gmail.com>
2024-12-24 18:51:19 +0200

    fix error

--- src/app/(admin)/admin/blog/[slug]/[PostSlug]/page.tsx
+++ src/app/(admin)/admin/blog/[slug]/[PostSlug]/page.tsx
@@ -1,12 +1,12 @@
-
 import BlogPost from '@/components/fPostadmin/BlogPost';
 import { notFound } from 'next/navigation';
 import React from 'react';
+
 interface Postsecondsubsection {
   secondtitle: string;
   description: string;
   imageUrl?: string;
-  imageFile?: File; // Temporary property to store the selected file before upload
+  imageFile?: File;
 }
 
 interface Postfirstsubsection {
@@ -14,68 +14,60 @@ interface Postfirstsubsection {
   description: string;
   Postsecondsubsections: Postsecondsubsection[];
   imageUrl?: string;
-  imageFile?: File; // Temporary property to store the selected file before upload
+  imageFile?: File;
 }
 
-interface blog {
+interface Blog {
   title: string;
   description: string;
   Postfirstsubsections: Postfirstsubsection[];
-  blogCategory: blogCategory;
+  blogCategory: BlogCategory;
   imageUrl?: string;
-  user:User;
-  createdAt:string;
+  user: User;
+  createdAt: string;
 }
-interface User{
- _id:string;
- username:string
+
+interface User {
+  _id: string;
+  username: string;
 }
-interface blogCategory {
+
+interface BlogCategory {
   _id: string;
   name: string;
 }
 
-interface blogCategory{
-  _id:string
-  name:string
+const fetchBlogData = async (id: string): Promise<Blog> => {
+  const res = await fetch(`${process.env.NEXTAUTH_URL}/api/blog/admin/PostBySlugAdmin/${id}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    next: { revalidate: 0 },
+  });
+
+  if (!res.ok) {
+    notFound();
+  }
+
+  const data: Blog = await res.json();
+  return data;
+};
+
+type Params = Promise<{ PostSlug: string }>;
+
+export default async function Page({ params }: { params: Params }) {
+  const { PostSlug: id } = await params;
+
+  if (!id) {
+    return notFound();
+  }
+
+  const blog = await fetchBlogData(id);
+
+  return (
+    <div>
+      <BlogPost blog={blog} />
+    </div>
+  );
 }
-
-const fetchBlogData = async (id: string): Promise<blog> => {
-  
-      const res = await fetch(`${process.env.NEXTAUTH_URL}/api/blog/admin/PostBySlugAdmin/${id}`, {
-        method: 'GET',
-        headers: {
-          "Content-Type": "application/json",
-        },
-        next: { revalidate: 0 }, // Disable caching
-      });
-  
-      if (!res.ok) {
-        notFound();
-      }
-  
-      const data: blog = await res.json();
-    
-      return data;
-    
-  };
-  
-  export default async function Page({ params }: { params: {PostSlug: string } }) {
-
-    const {PostSlug:id} = params;
-    // Early return if no product id
-    if (!id) {
-      return notFound();
-    }
-
-    const blog = await fetchBlogData(id);
-    
-   
-    return (
-        <div>
-            <BlogPost blog={blog} />
-        </div>
-    );
-}
-
-
