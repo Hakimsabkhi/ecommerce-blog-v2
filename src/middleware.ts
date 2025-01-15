f31334b0c80c39c197353e24a39740f097b877fc
unknown <hakimsabkhi001@gmail.com>
2025-01-15 02:33:04 -0800

    fix middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -26,49 +26,12 @@ export async function middleware(req: NextRequest) {
   if (token && isAuthPath) {
     return NextResponse.redirect(new URL('/', req.url));
   }
-  if (req.nextUrl.pathname.startsWith('/orderhistory')|| 
-  req.nextUrl.pathname.startsWith('/checkout')||req.nextUrl.pathname.startsWith('/settings')) {
-    const userRole = token?.role;
-  
-    if (userRole !== 'SuperAdmin' && userRole !== 'Admin'&& userRole !== 'Consulter' && userRole !== 'Visiteur') {
-      return NextResponse.redirect(new URL('/', req.url));
-    }
-  }
 
-  if (req.nextUrl.pathname.startsWith('/admin')) {
-    const userRole = token?.role;
-  
-    if (userRole !== 'SuperAdmin' && userRole !== 'Admin'&& userRole !== 'Consulter') {
-      return NextResponse.redirect(new URL('/', req.url));
-    }
-  }
-// If the path is /admin, check the user's role
-if (req.nextUrl.pathname.startsWith('/admin/users')) {
-  const userRole = token?.role;
-
-  if (userRole !== 'SuperAdmin' && userRole !== 'Admin') {
-    return NextResponse.redirect(new URL('/', req.url));
-  }
-}
-
-if ( req.nextUrl.pathname.startsWith('/admin/category') || 
-req.nextUrl.pathname.startsWith('/admin/product')||
-req.nextUrl.pathname.startsWith('/admin/brand')||
-req.nextUrl.pathname.startsWith('/admin/review')||
-req.nextUrl.pathname.startsWith('/admin/order')
-) {
-  const userRole = token?.role;
-
-  if (userRole !== 'SuperAdmin' && userRole !== 'Admin'&& userRole !== 'Consulter') {
-    return NextResponse.redirect(new URL('/', req.url));
-  }
-}
   return NextResponse.next();
 }
 
 export const config = {
   matcher: [
-    '/admin/:path*',
     '/orderhistory',
     '/checkout',
     '/settings'
