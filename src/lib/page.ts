9df9c7aa4e8ab261171967f7ab5a346d450e0828
Mohamed Salah Bayoudh <mohamed.salah.bayoudh@gmail.com>
2025-01-16 14:11:27 +0100

    company

--- src/lib/page.ts
+++ src/lib/page.ts
@@ -1,37 +1,48 @@
-export type Page = 'Brands' | 'Categories' | 'Products' | 'Promation' | 'Reviews' | 'Orders' | 'Invoice' | 'Revenue' | 'Blog' | 'B.Comments'| 'Post Category' |'Entreprise';
+export enum Page {
+  Users = "Users",
+  Brands = "Brands",
+  Categories = "Categories",
+  Products = "Products",
+  Promotion = "Promotion",
+  Reviews = "Reviews",
+  Orders = "Orders",
+  Invoice = "Invoice",
+  Revenue = "Revenue",
+  Blog = "Blog",
+  BlogComments = "BlogComments",
+  PostCategory = "PostCategory",
+  Company = "Company",
+  Role = "Role",
+  Entreprise="Entreprise",
+}
 
-export const pages: Page[] = [
-  'Brands', 
-  'Categories', 
-  'Products', 
-  'Promation', 
-  'Reviews', 
-  'Orders', 
-  'Invoice', 
-  'Revenue', 
-  'Blog', 
-  'B.Comments', 
-  'Post Category',
-  'Entreprise'
-];
+// Define the main structure with nested groups
+export const pages = {
+  "Website Info": [{ name: Page.Company, path: "/admin/company" }],
+  "Gestion Stocks": [
+    { name: Page.Brands, path: "/admin/brand" },
+    { name: Page.Categories, path: "/admin/category" },
+    { name: Page.Products, path: "/admin/product" },
+    { name: Page.Promotion, path: "/admin/promotion" },
+  ],
+  "Manage Users": [
+    { name: Page.Users, path: "/admin/users" },
+    { name: Page.Role, path: "/admin/users/role" },
+  ],
 
-// Map pages to URLs for the admin panel
-export  const pageUrls = [
-    { name: 'Users', path: '/admin/users' },
-    { name: 'Brands', path: '/admin/brand' },
-    { name: 'Categories', path: '/admin/category' },
-    { name: 'Products', path: '/admin/product' },
-    { name: 'Promation', path: '/admin/promotion' },
-    { name: 'Reviews', path: '/admin/review' },
-    { name: 'Orders', path: '/admin/order' },
-    { name: 'Invoice', path: '/admin/invoice' },
-    { name: 'Company', path: '/admin/company' },
-    { name: 'Revenue', path: '/admin/revenue' },
-    { name: 'Role', path: '/admin/users/role' },
-    { name: 'Blog', path: '/admin/blog' },
-    { name: 'B.Comments', path: '/admin/bloglike' },
-    { name: 'Post Category', path: '/admin/postcategory' },
-    { name: 'Entreprise', path: '/admin/companies' }
-    
-  ];
+  "Clients Feedbacks": [{ name: Page.Reviews, path: "/admin/review" }],
+  "Lists Orders": [
+    { name: Page.Orders, path: "/admin/order" },
+    { name: Page.Invoice, path: "/admin/invoice" },
+    { name: Page.Revenue, path: "/admin/revenue" },
+  ],
+  Blog: [
+    { name: Page.Blog, path: "/admin/blog" },
+    { name: Page.BlogComments, path: "/admin/blog/comments" },
+    { name: Page.PostCategory, path: "/admin/post-category" },
+  ],
+  "Entreprise": [
+    { name: Page.Entreprise, path: '/admin/companies' }
+  ],
+};
   
