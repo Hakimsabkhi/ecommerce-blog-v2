253641ffcce48510b40730ed64c9b430515b404d
Mohamed Salah Bayoudh <mohamed.salah.bayoudh@gmail.com>
2025-01-16 15:43:01 +0100

    page

--- src/app/admin/blog/post-category/[updatecategory]/page.tsx
+++ src/app/admin/blog/post-category/[updatecategory]/page.tsx
@@ -75,7 +75,7 @@ const ModifyCategory = () => {
       }
    
       toast.success(`Category ${categoryData.name} modification successfully!`);
-    router.push('/admin/post-category')
+    router.push('/admin/blog/post-category')
     } catch (error) {
       toast.error(`Error: ${error instanceof Error ? error.message : 'Unknown error'}`);
     }
@@ -106,7 +106,7 @@ const ModifyCategory = () => {
           </button>
         </div>
         <div className="w-[20%] max-xl:w-[30%] max-md:w-[50%] items-start">
-          <Link href="/admin/post-category">
+          <Link href="/admin/blog/post-category">
             <button className="bg-white border-2 border-gray-400 text-black hover:bg-gray-600 hover:text-white hover:border-0 rounded-md w-full h-10 flex items-center justify-center">
               <p className="font-bold">Cancel</p>
             </button>
