7559fd463089b3bc4b7f162949ad3c288409d187
Mohamed Salah Bayoudh <mohamed.salah.bayoudh@gmail.com>
2025-01-20 16:39:11 +0100

    acess

--- src/app/api/role/getrole/route.ts
+++ src/app/api/role/getrole/route.ts
@@ -5,7 +5,20 @@ import Role from '@/models/Role';
 export async function GET() {
   await connectToDatabase();
   try {
-    const roles = await Role.find({ name: { $nin: ['Admin', 'SuperAdmin'] }}, { name: 1, access: 1 }).lean();
+    const role = await Role.find({ name: { $nin: [ 'SuperAdmin'] }}, { name: 1, access: 1 }).lean();
+    const adminExists = role.some(role => role.name === 'Admin');
+    const visitorExists = role.some(role => role.name === 'Visiteur');
+    if(!adminExists){
+      const role = new Role({ name: 'Admin', access: {} });
+      await role.save();
+    }
+    if(!visitorExists){
+      const role = new Role({ name: 'Visiteur', access: {} });
+      await role.save();
+    }
+    const roles = await Role.find({ name: { $nin: [ 'SuperAdmin'] }}, { name: 1, access: 1 }).lean();
+    console.log('Admin exists:', adminExists);
+    console.log('Visitor exists:', visitorExists);
     return NextResponse.json({ roles });
   } catch (err) {
     console.log(err)
